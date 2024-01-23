4df1f63654784e649773a3d0acb937897fefeb0d
Priscila Da Dalt <priscila.dalt@aluno.faculdadeimpacta.com.br>
2024-01-23 16:17:05 -0300

    Adicionar arquivos

---
+++ Consultar-ultima-alteracao-senha-azure-ad.ps1
@@ -0,0 +1,7 @@
+﻿#accept any prompts for untrusted repositories 
+Install-module MSOnline
+
+#then enter your O365 Global Admin details
+Connect-MSOLService 
+
+Get-MsolUser -UserPrincipalName 'ava527@einstein.br' | Select LastPasswordChangeTimestamp
---
+++ Limpar_lixeira_site.ps1
@@ -0,0 +1,8 @@
+﻿
+Connect-PnPOnline -Url "https://5phs4v.sharepoint.com/sites/Teste1" -UseWebLogin
+
+Clear-PnPRecycleBinItem
+
+
+
+	
